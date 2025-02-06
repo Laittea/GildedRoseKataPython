562f60acde92d4f14b65b98703482d9a017c7ba9
Wenxin Zhu <zhu.wenxi@northeastern.edu>
2025-02-05 21:56:08 -0800

    Complete lab04

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -21,8 +21,57 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose = GildedRose(items)
         all_items = gilded_rose.get_item()
         self.assertEqual(["Sulfuras"], all_items)
+        
+    
 
+    # #First logical errors
+    def test_normal_item_decreases_in_quality(self):
+        items = [Item("+5 Dexterity Vest", 5, 10)]  
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        
+        self.assertEqual(items[0].quality, 8)  
+        self.assertEqual(items[0].sell_in,3)  
+        
+    # #Second logical errors
+    def test_aged_brie_increases_in_quality(self):
+        items = [Item("Aged Brie", 2, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
 
+        self.assertEqual(items[0].quality, 12)  
+        self.assertEqual(items[0].sell_in, 1)  
+        
+    # Third logical errors
+    def test_backstage_passes_quality_drops_to_zero(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 30)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+
+        self.assertEqual(items[0].quality, 29)  
+    
+    # Fourth logical errors
+    def test_conjured_mona_cake_decrease_in_quality(self):
+        items = [Item("Conjured Mona Cake", 2, 30)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        
+        self.assertEqual(items[0].quality, 25)
+    
+    
+    #Syntax error
+    def test_gilded_rose_list_all_items_again(self):
+        items = [Item("Aged Brie", 5, 80)]
+        gilded_rose = GildedRose(items)
+        all_items = gilded_rose.get_item()
+        self.assertEqual(["Aged Brie"], all_items)
+    
+    #Second Syntax error
+    def test_gilded_rose_conjured_mona_cake(self):
+        items = [Item("Conjured Mona Cake", 5, 80)]
+        gilded_rose = GildedRose(items)
+        all_items = gilded_rose.sell_item()
+        self.assertEqual(["Conjured Mona Cake"], all_items)
 
 if __name__ == '__main__':
     unittest.main()
